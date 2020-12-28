8a3335a815bc71a4a6e6962119b24230cc4b62a4
Ataman <ataman_kunbuk@hotmail.com>
2020-12-28 21:21:26 +0300

    New Commit

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,115 +1,104 @@
-var footer="Asım Sinan Yüksel 2020"
-const anaSayfa=function(req, res, next) {
-  res.render('mekanlar-liste', 
-  	{
-      'baslik': 'Anasayfa',
-      'footer':footer,
-  	'sayfaBaslik':{
-  		'siteAd':'Mekan32',
-  		'aciklama':'Isparta civarındaki mekanları keşfedin'
-  	},
-    'mekanlar':[
-    {
-      'ad':'Starbucks',
-      'adres':'Centrum Garden AVM',
-      'puan':3,
-      'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-      'mesafe':'10km'
+var express = require("express");
+var router = express.Router();
+
+const anaSayfa = function (req, res, next) {
+  res.render("mekanlar-liste", {
+    baslik: "Anasayfa",
+    sayfaBaslik: {
+      siteAd: "Mekan32",
+      aciklama: "Isparta civarındaki mekanları keşfedin!",
     },
+    footer: "Kerem Kocaekiz",
+    mekanlar: [
       {
-      'ad':'Hell-Hot Fast Food',
-      'adres':'SDÜ Doğu Kampüsü',
-      'puan':5,
-      'imkanlar':['Dürüm','Hamburger','Sosisli'],
-      'mesafe':'1km'
-        },
-          {
-            'ad': 'Benim Adım İSkender',
-            'adres': 'Çünür',
-            'puan': 3,
-            'imkanlar': ['İskender', 'Köfte', 'Kebap'],
-            'mesafe': '10km'
-        },
-        {
-            'ad': 'For Sale Pub',
-            'adres': 'Çünür',
-            'puan': 3,
-            'imkanlar': ['Alkollü İçecek', 'Alkolsüz İçecek', 'Fıstık'],
-            'mesafe': '10km'
-        },
-        {
-            'ad': 'The Pastane',
-            'adres': 'Çünür',
-            'puan': 3,
-            'imkanlar': ['Pasta', 'Börek', 'Çay'],
-            'mesafe': '10km'
-        }
-    ]
-  }
-
-  	);
-}
-
-const mekanBilgisi=function(req, res, next) {
-  res.render('mekan-detay', 
-    { 
-    'baslik': 'Mekan Bilgisi',
-    'footer':footer,
-    'sayfaBaslik':'Starbucks',
-    'mekanBilgisi':{
-      'ad':'Starbucks',
-      'adres':'Centrum Garden AVM',
-      'puan':3,
-      'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-      'koordinatlar':{
-          'enlem':'37.781885',
-          'boylam':'30.566034'
+        ad: "Starbucks",
+        adres: "Centrum Garden",
+        puan: "3",
+        imkanlar: ["kahve", "çay", "pasta"],
+        mesafe: "10km",
       },
-      'saatler':[
+      {
+        ad: "Gloria Jeans",
+        adres: "Iyaş AVM",
+        puan: "2",
+        imkanlar: ["kahve", "kek", "çay"],
+        mesafe: "5km",
+      },
+      {
+        ad: "Sarımsak Gurme",
+        adres: "Centrum Garden",
+        puan: "5",
+        imkanlar: ["Yemek", "Tatlı", "İçecek"],
+        mesafe: "10km",
+      },
+      {
+        ad: "Iyaş Bulvar",
+        adres: "Iyaş Bulvar",
+        puan: "2",
+        imkanlar: ["Alışveriş", "Eğlence", "Yemek"],
+        mesafe: "8km",
+      },
+      {
+        ad: "Kaktüs Cadde",
+        adres: "Iyaş AVM",
+        puan: "5",
+        imkanlar: ["Yemek", "Tatlı", "İçecek"],
+        mesafe: "2km",
+      },
+    ],
+  });
+};
+
+const mekanBilgisi = function (req, res, next) {
+  res.render("mekan-detay", {
+    baslik: "Mekan Bilgisi",
+    sayfaBaslik: "Starbucks",
+    footer: "Kerem Kocaekiz",
+    mekanBilgisi: {
+      ad: "Starbucks",
+      adres: "Centrum Garden",
+      puan: "3",
+      imkanlar: ["kahve", "çay", "pasta"],
+      koordinatlar: {
+        enlem: 37.781885,
+        boylam: 30.566034,
+      },
+      saatler: [
         {
-          'gunler':'Pazartesi-Cuma',
-          'acilis':'7:00',
-          'kapanis':'23:00',
-          'kapali':false
+          gunler: "Pazartesi-Cuma",
+          acilis: "7:00",
+          kapanis: "23:00",
+          kapali: false,
         },
         {
-          'gunler':'Cumartesi',
-          'acilis':'9:00',
-          'kapanis':'22:00',
-          'kapali':false
+          gunler: "Cumartesi",
+          acilis: "9:00",
+          kapanis: "22:30",
+          kapali: false,
         },
         {
-          'gunler':'Pazar', 
-          'kapali':true
-        }
+          gunler: "Pazar",
+          kapali: true,
+        },
       ],
-      'yorumlar':[
+      yorumlar: [
         {
-          'yorumYapan':'Asım Sinan Yüksel',
-          'puan':3,
-          'tarih':'27.11.2020',
-          'yorumMetni':'Kahveleri güzel.'
+          yorumYapan: "Kerem Kocaekiz",
+          puan: "3",
+          tarih: "1 Aralık 2020",
+          yorumMetni: "Kahveleri Çok Kötü",
         },
-        {
-          'yorumYapan':'asass',
-          'puan':4,
-          'tarih':'27.11.2020',
-          'yorumMetni':'Kahvelerasasl.'
-        }
-        
-      ]
+      ],
+    },
+  });
+};
 
-    }
-  }
-    );
-}
+const yorumEkle = function (req, res, next) {
+  res.render("yorum-ekle", { title: "Yorum Ekle" });
+};
 
-const yorumEkle=function(req, res, next) {
-  res.render('yorum-ekle', { title: 'Yorum Ekle' });
-}
-
-module.exports={
-anaSayfa,
-mekanBilgisi,
-yorumEkle
-}
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+};
